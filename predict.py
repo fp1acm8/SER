8766036c234ddbf68ea4a27d5f76e290ce6257ee
Francesco Pucci <95233773+fp1acm8@users.noreply.github.com>
2021-12-14 10:27:12 +0100

    commit on 'main'

--- predict.py
+++ predict.py
@@ -1,19 +1,24 @@
 import pandas as pd
 import numpy as np
+from numpy import loadtxt
+
 
 from joblib import load
 
 from models.mlp import MLPClassifier
 
 def predict(X, Y, encoder, model):
-    y_pred = model.predict(X,encoder)
+
+    model.predict(X,encoder)
+    model.confusion_matrix(X, Y, encoder)
+    model.classification_report(X, Y, encoder)
 
 
 if __name__ == '__main__':
     # Setup
-    X = pd.read_csv('checkpoints/x_test.csv') #load features
-    Y = pd.read_csv('checkpoints/y_test.csv') #load labels
+    X = loadtxt('checkpoints/x_test.csv', delimiter=',') #load features
+    Y = loadtxt('checkpoints/y_test.csv', delimiter=',') #load labels
     encoder = load('checkpoints/encoder.joblib') #load encoder
-    model = MLPClassifier.load(path='models', name='trained_model')
+    model = MLPClassifier.load(path='models', name='trained_model') #load trained model
 
-    y_pred = predict(X, Y, encoder, model)
+    predict(X, Y, encoder, model)
